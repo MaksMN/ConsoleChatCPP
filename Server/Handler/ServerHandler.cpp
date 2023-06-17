ea88a6d5e6b1e28d69b1ef46cc58aacfeacf0bff
MaksMN <maksimovmn78@gmail.com>
2023-06-17 20:38:10 +0300

    Проработана новая концепция

--- Server/Handler/ServerHandler.cpp
+++ Server/Handler/ServerHandler.cpp
@@ -20,58 +20,72 @@ void ServerHandler::InitialiseDB()
 void ServerHandler::Run()
 {
     /*
-    Входящий пакет
-    |session_key|login_size|login|page_size|    PAGE_TEXT    |size| CMD_TEXT        |
-                8         12     |12+login_size              |pagePos + 4 + pageSize
 
-    Исходящий пакет команд
-    |I - Запрос числа|clear 0-1|size|PAGE_TEXT|
-                               2
+    Командный пакет
+    |Запрос у клиента I/S (1)|clear 0-1 (1)|session_key (8)|login_size(4)|login|page_size(4)|PAGE_TEXT|cmd_size(4)|CMD_TEXT|
+    0                        1             2               10                  1                      2
+    |  static                                              | dynamic
+    Запрос у клиента: I - число S - строка
+    Если ожидается ввод числа, записываем его в блок cmd_size
+
     */
-    session_key = Misc::getLong(cmd_buffer);
-    login_size = Misc::getInt(cmd_buffer, 8);
 
-    if (12 + login_size >= CMD_BUFFER || pageSize + 4 > CMD_BUFFER)
+    auto session_key = Misc::getLong(cmd_buffer, 2);
+
+    auto login_size = Misc::getInt(cmd_buffer, 10);
+    if (login_size >= CMD_BUFFER)
     {
         badRequest();
         return;
     }
 
-    pagePos = 12 + login_size;
-    pageSize = Misc::getInt(cmd_buffer, pagePos);
-    if (pageSize + 4 >= CMD_BUFFER)
+    auto page_pos = Misc::findDynamicData(cmd_buffer, 10, 1);
+    if (page_pos == 0)
     {
         badRequest();
         return;
     }
 
-    cmdPos = pagePos + 4 + pageSize;
-    cmdSize = Misc::getInt(cmd_buffer, cmdPos);
-    if (cmdSize + 4 > CMD_BUFFER)
+    auto cmd_pos = Misc::findDynamicData(cmd_buffer, 10, 2);
+    if (cmd_pos == 0)
     {
         badRequest();
         return;
     }
 
-    login = Misc::getString(cmd_buffer, 8);
-    pageText = Misc::getString(cmd_buffer, pagePos);
-    cmdText = Misc::getString(cmd_buffer, cmdPos);
+    auto login = Misc::getString(cmd_buffer, 10);
+    auto page_text = Misc::getString(cmd_buffer, page_pos);
+    auto cmd_text = Misc::getString(cmd_buffer, cmd_pos);
 
+    // Запишем в буфер данные на случай если при обработке данные не изменятся.
+    Misc::writeStringBuffer("Вы ввели неизвестную команду.\nВведите команду: ", data_buffer);
+
+    user = usersDB.getUserByLogin(login);
+
+    if ((user != nullptr && user->getSessionKey() != session_key) || user == nullptr)
+    {
+        user = nullptr;
+        login = "Guest";
+    }
+
+    if (user == nullptr)
+    {
+        ChatGuestPage guestPage{pubMessagesDB, privMessagesDB, complaintsDB, usersDB, data_buffer, cmd_buffer};
+        guestPage.run(pageText, cmdText);
+        // return;
+    }
     if (cmdText.compare("HELLO") == 0)
     {
-        Misc::writeStringBuffer("00", cmd_buffer, 0, false);
-        Misc::writeStringBuffer(pageText, cmd_buffer, 2);
-        Misc::writeStringBuffer("Привет, " + login + "! Я сервер.\nВведите команду: ", data_buffer);
-
+        Misc::writeStringBuffer("Привет, " + login + "! Я сервер, я живой.\nВведите команду: ", data_buffer);
         return;
     }
 
+    // Запишем в буфер данные если ни одна из команд не попала под условия обработки.
+    Misc::writeStringBuffer("Вы ввели неизвестную команду.\nВведите команду: ", data_buffer);
     return;
 }
 
 void ServerHandler::badRequest()
 {
-    Misc::writeStringBuffer("01", cmd_buffer, 0, false);
-    Misc::writeStringBuffer("WRONG", cmd_buffer, 2);
-    Misc::writeStringBuffer("Wrong command", data_buffer);
+    Misc::writeStringBuffer("Что-то пошло не так. На сервер пришли данные неверной длинны.", data_buffer);
 }
