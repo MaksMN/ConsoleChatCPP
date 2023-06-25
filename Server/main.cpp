c90a2eaccdcac9e1eefd85b3d1cbd9904dbcf71c
MaksMN <maksimovmn78@gmail.com>
2023-06-25 17:58:16 +0300

    Небольшие доработки

--- Server/main.cpp
+++ Server/main.cpp
@@ -4,6 +4,8 @@
 #if defined(_WIN64) || defined(_WIN32)
 
 #include "WinServer/WinServer.h"
+#include <io.h>
+#include <fcntl.h>
 
 #elif defined(__linux__)
 
@@ -15,6 +17,10 @@ typedef unsigned int uint;
 
 int main(int argc, const char *argv[])
 {
+#if defined(_WIN64) || defined(_WIN32)
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+#endif
     /**
      * Порт можно указать в параметрах запуска
      * Если ничего не указано, порт = 7777
@@ -30,9 +36,17 @@ int main(int argc, const char *argv[])
     }
 
     const auto PORT = port_s.data();
+#if defined(_WIN64) || defined(_WIN32)
+    std::wcout << L"SERVER IS LISTENING THROUGH THE PORT: " << PORT << " WITHIN A LOCAL SYSTEM" << std::endl;
+    std::wcout << L"Сервисный администратор: логин - admin пароль - 1234" << std::endl;
+    std::wcout << L"Авторизуйтесь в клиенте и смените пароль командой /profile" << std::endl;
+    std::wcout << L"Чтобы завершить работу сервера, авторизуйтесь в клиенте с администраторскими правами и введите команду /sv_quit" << std::endl;
+#elif defined(__linux__)
     std::cout << "SERVER IS LISTENING THROUGH THE PORT: " << PORT << " WITHIN A LOCAL SYSTEM" << std::endl;
-    std::cout << "Сервисный администратор: логин-admin пароль-1234" << std::endl;
+    std::cout << "Сервисный администратор: логин - admin пароль - 1234" << std::endl;
     std::cout << "Авторизуйтесь в клиенте и смените пароль командой /profile" << std::endl;
+    std::cout << "Чтобы завершить работу сервера, авторизуйтесь в клиенте с администраторскими правами и введите команду /sv_quit" << std::endl;
+#endif
 
     server_socket(PORT);
 
