43e83fc8f3ecf1978e688472d5e61847b3a86ddc
MaksMN <maksimovmn78@gmail.com>
2023-08-19 13:40:51 +0300

    update make files

--- make_serverWin.cmd
+++ make_serverWin.cmd
@@ -2,6 +2,6 @@ echo off
 mkdir .\bin
 cls
 echo Build server...
-g++ -std=c++20 %cd%/Server/*.cpp %cd%/Server/DBclient/*.cpp %cd%/Server/ChatCore/*.cpp %cd%/Server/Socket/Handler/*.cpp %cd%/Server/Socket/WinServer/*.cpp %cd%/Misc/*.cpp %cd%/Server/ChatPages/*.cpp "-Ic:\Program Files\MySQL\MySQL Server 8.0\include" "-Lc:\Program Files\MySQL\MySQL Server 8.0\lib" -llibmysql -lws2_32 -lodbc32 -o %cd%\bin\server.exe
+g++ -std=c++20 %cd%/Server/*.cpp %cd%/Server/DBclient/*.cpp %cd%/Server/ChatCore/*.cpp %cd%/Server/Socket/Handler/*.cpp %cd%/Server/Socket/WinServer/*.cpp %cd%/Misc/*.cpp %cd%/Server/ChatPages/*.cpp %cd%/Server/Socket/Logger/*.cpp %cd%/Server/Socket/SVCLI/*.cpp "-Ic:\Program Files\MySQL\MySQL Server 8.0\include" "-Lc:\Program Files\MySQL\MySQL Server 8.0\lib" -llibmysql -lws2_32 -lodbc32 -o %cd%\bin\server.exe
 echo Build server complete
 pause
